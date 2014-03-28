e4526b21c7b4cc493410f2e1e0e3c3ba8bdffd33
dhardy92 <dhardy@viadeoteam.com>
2014-03-28 17:26:27 +0100

    [deb] use us mirror

--- pbuilder_scripts/create.sh
+++ pbuilder_scripts/create.sh
@@ -2,7 +2,7 @@
 mkdir -p pbuilder/build
 sudo pbuilder create \
   --distribution squeeze \
-  --mirror ftp://ftp.fr.debian.org/debian/ \
+  --mirror ftp://ftp.us.debian.org/debian/ \
   --othermirror "deb http://debrepo01.sf.viadeo.local/debian squeeze main contrib non-free|deb http://debrepo01.sf.viadeo.local/debian demo main contrib non-free|deb http://backports.debian.org/debian-backports squeeze-backports main contrib non-free" \
   --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg" \
   --basetgz ./squeeze64-viadeo.tgz \
