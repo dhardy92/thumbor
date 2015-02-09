4f12199c7f80c73ba79c9ce207d68af35a2cb9e3
dhardy92 <dhardy@viadeoteam.com>
2015-02-09 14:04:03 +0100

    [debian] use pbuilder + add new dependencies +  release 4.8.6

---
+++ pbuilder_scripts/create.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p pbuilder/build
+sudo pbuilder create \
+  --distribution squeeze \
+  --mirror ftp://ftp.us.debian.org/debian/ \
+  --othermirror "deb http://debrepo01.sf.viadeo.local/debian squeeze main contrib non-free|deb http://debrepo01.sf.viadeo.local/debian demo main contrib non-free|deb http://backports.debian.org/debian-backports squeeze-backports main contrib non-free" \
+  --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg" \
+  --basetgz ./squeeze64-viadeo.tgz \
+  --buildplace pbuilder/build \
+  --aptcache "" \
+  --architecture "amd64"
