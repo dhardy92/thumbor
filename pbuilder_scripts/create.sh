d838a3e31fb5b504f34cb7fa669157f03aa8dabe
dhardy92 <dhardy@viadeoteam.com>
2015-02-17 15:49:04 +0100

    [debian] wheezy repos

--- pbuilder_scripts/create.sh
+++ pbuilder_scripts/create.sh
@@ -3,7 +3,7 @@ mkdir -p pbuilder/build
 sudo pbuilder create \
   --distribution wheezy \
   --mirror ftp://ftp.us.debian.org/debian/ \
-  --othermirror "deb http://debrepo01.sf.viadeo.local/debian squeeze main contrib non-free|deb http://debrepo01.sf.viadeo.local/debian demo main contrib non-free|deb http://backports.debian.org/debian-backports squeeze-backports main contrib non-free" \
+  --othermirror "deb http://debrepo01.sf.viadeo.local/debian wheezy main contrib non-free|deb http://debrepo01.sf.viadeo.local/debian demo main contrib non-free|deb http://backports.debian.org/debian-backports wheezy-backports main contrib non-free" \
   --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg" \
   --basetgz ./wheezy64-viadeo.tgz \
   --buildplace pbuilder/build \
