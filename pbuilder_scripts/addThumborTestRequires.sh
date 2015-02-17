5ae7df4cca6c069f5d7a58a777062420ee065960
dhardy92 <dhardy@viadeoteam.com>
2015-02-17 17:00:11 +0100

    [debian] fix thumbor build pip prerequisits

--- pbuilder_scripts/addThumborTestRequires.sh
+++ pbuilder_scripts/addThumborTestRequires.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-su - -c "apt-get -y install libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev & pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' 'coverage' 'nose' 'colorama' 'numpy'"
+apt-get -y install libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev && \
+pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' 'coverage' 'nose' 'colorama' 'numpy'
