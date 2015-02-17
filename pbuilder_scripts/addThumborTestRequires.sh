96354bc4dcbe8f332671c5d0922b8bd9f14d1611
dhardy92 <dhardy@viadeoteam.com>
2015-02-17 15:18:39 +0100

    [debian] add curl-config to build pycurl

--- pbuilder_scripts/addThumborTestRequires.sh
+++ pbuilder_scripts/addThumborTestRequires.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-su - -c "apt-get -y install libxslt-dev libxml2-dev python-pip python-pycurl python-dev && pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' 'coverage' 'nose' 'colorama' 'numpy'"
+su - -c "apt-get -y install libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev & pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' 'coverage' 'nose' 'colorama' 'numpy'"
