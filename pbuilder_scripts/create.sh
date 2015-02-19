7f6413bf8b8cb0d00f97f688b6d495a3ef50015b
dhardy92 <dhardy@viadeoteam.com>
2015-02-19 11:57:56 +0100

    [deb] use extrapackage directive on create pbuilder

--- pbuilder_scripts/create.sh
+++ pbuilder_scripts/create.sh
@@ -6,6 +6,7 @@ sudo pbuilder create \
   --othermirror "deb http://debrepo01.sf.viadeo.local/debian wheezy main contrib non-free|deb http://debrepo01.sf.viadeo.local/debian demo main contrib non-free|deb-src http://ftp.debian.org/debian/ jessie main contrib non-free" \
   --debootstrapopts "--keyring=$PWD/pbuilder_scripts/keyring.gpg" \
   --basetgz ./wheezy64-viadeo.tgz \
+  --extrapackages devscripts libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev rubygems ruby1.8-dev fakeroot build-essential \
   --buildplace pbuilder/build \
   --aptcache "" \
   --architecture "amd64"
