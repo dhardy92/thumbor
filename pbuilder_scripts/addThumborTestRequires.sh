bf4a5e0c4486b4c9caf536700df76ece449d8d38
dhardy92 <dhardy@viadeoteam.com>
2015-02-19 10:28:10 +0100

    [deb] add fakeroot and build-essential

--- pbuilder_scripts/addThumborTestRequires.sh
+++ pbuilder_scripts/addThumborTestRequires.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get -y install devscripts libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev rubygems ruby1.8-dev && \
+apt-get -y install devscripts libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev rubygems ruby1.8-dev fakeroot build-essential && \
 pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' 'coverage' 'nose' 'colorama' 'numpy' && \
 apt-get source libwebp && \
 ( cd libwebp* && apt-get build-dep -y libwebp && debuild -i -us -uc -b ) && \
