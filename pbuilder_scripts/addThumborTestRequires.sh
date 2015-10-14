355333e9c6fc6ab37ce590026357f2910762b68b
Guilherme Souza <guivideojob@gmail.com>
2015-10-14 10:31:51 -0300

    [deb] 5.2.0

---
+++ pbuilder_scripts/addThumborTestRequires.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+apt-get -y install devscripts libxslt-dev libxml2-dev python-pip python-pycurl python-dev libcurl4-openssl-dev rubygems ruby1.8-dev fakeroot build-essential python-six python-crypto && \
+pip install 'simplejson>=2.1.6,<2.2.0' 'pyvows' 'preggy' 'tornado-pyvows' "tornado>=4.1.0,<5.0.0" 'coverage' 'nose' 'colorama' 'numpy' && \
+apt-get source libwebp && \
+( cd libwebp* && apt-get build-dep -y libwebp && debuild -i -us -uc -b ) && \
+dpkg -i libwebp5*.deb libwebp-dev*.deb libwebpdemux1*.deb libwebpmux1*.deb && \
+gem install fpm && \
+for i in pillow statsd pexif derpconf libthumbor; do
+  fpm -s python -t deb --no-python-dependencies $i && dpkg -i ./python-${i}_*.deb
+done
+
+exit 0
